5c6a6287cf6e1cf469c0acf7fa627cdc8f0cbf6b
Shrikant-Meshram <mshrikantr99@gmail.com>
2022-04-18 00:13:36 +0530

    Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const BookController= require("../controllers/BookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/getBooksInYear", BookController.getBooksInYear  )
+
+router.get("/getXINRBooks",BookController.getXINRBooks)
+
+router.get("/getUsersData", UserController.getUsersData)
+
+router.post("/getParticularBook", BookController.getParticularBook  )
+
+router.get("/bookList", BookController.bookList)
+router.get("/getRandomBooks", BookController.getRandomBooks)
+
+router.get("/getBookData", BookController.getBookData)
 
 module.exports = router;
