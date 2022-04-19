8b627dc1d73ada8dcd61345c00315d2cf6a3984b
Shrikant-Meshram <mshrikantr99@gmail.com>
2022-04-20 00:33:04 +0530

    19/04_Assignment

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
+        const currentDate = new Date().toLocaleString()
         console.log ("inside GLOBAL MW");
+        console.log (currentDate,req.ip,req.originalUrl)
+
         next();
   }
   );
